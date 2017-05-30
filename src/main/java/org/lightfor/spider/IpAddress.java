6875b44b925e374396fa8c6ba7e3b160f512e674
lightfor <death_light@foxmail.com>
2017-05-30 21:16:25 +0800

    add the proxy code, change the ip assignment method

--- src/main/java/org/lightfor/spider/IpAddress.java
+++ src/main/java/org/lightfor/spider/IpAddress.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 /**
  * IP Location Info
@@ -13,12 +15,24 @@ public class IpAddress {
 
     public static void main(String[] args) throws IOException {
         String url = "http://www.ipip.net/ip.html";
-        String ip = "8.8.8.8";
-        Document doc = Jsoup.connect(url)
-                .data("ip", ip)
-                .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
-                .referrer(url)
-                .post();
-        System.out.println(doc.select("#myself").text());
+        String ipStr = "8.8.8.8\n" +
+                "8.8.4.4\n";
+        String[] ips = ipStr.split("\n");
+
+        //Proxy proxy = new Proxy( Proxy.Type.HTTP, InetSocketAddress.createUnresolved("127.0.0.1", 1080) );
+
+        //2. set system properties, not recommend
+        //System.getProperties().setProperty("http.proxyHost", "127.0.0.1");
+        //System.getProperties().setProperty("http.proxyPort", "1080");
+        for(String ip : ips){
+            Document doc = Jsoup.connect(url)
+                    .timeout(10000)
+                    //.proxy(proxy)
+                    .data("ip", ip)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer(url)
+                    .post();
+            System.out.println(doc.select("#myself").text());
+        }
     }
 }
